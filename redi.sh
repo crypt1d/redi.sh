915c7f1dcce6925089a4d8a4b183f0b7f1a5c447
Nikola Krzalic <nikola@krzalic.com>
2015-04-12 13:21:44 -0400

    Added pipping support and getopts argument parsing.

--- redi.sh
+++ redi.sh
@@ -5,24 +5,46 @@ REDIS_PORT=6379
 
 . "functions/redis.sh"
 
-function set_from_pipe() {
-	typeset KEYNAME=$(echo "$@" | awk -F\= '{print $1}')
-	typeset KEYVALUE=$(echo "$@" | awk -F\= '{print $2}')
 
-	redis_compose_cmd "SET $KEYNAME $KEYVALUE" >&3
-}
-
-function read_from_pipe() {
-	while read line
-	do
-		echo -ne $line
-	done < /dev/stdin
-}
-
+while getopts g:P:h:p opt; do
+	case $opt in
+		p)
+			REDIS_PW=${OPTARG}
+			;;
+		H)
+			REDIS_HOST=${OPTARG}
+			;;
+		P)
+			REDIS_PORT=${OPTARG}
+			;;
+		g)
+			REDIS_GET=${OPTARG}
+			;;
+	esac
+done
+	
 exec 3<> /dev/tcp/$REDIS_HOST/$REDIS_PORT
 
-redis_compose_cmd "GET test" >&3
+if [[ ! -z $REDIS_PW ]]; then
+	redis_compose_cmd "AUTH $REDIS_PW" >&3
+fi
 
+if [[ ! -z $REDIS_GET ]]; then
+	redis_compose_cmd "GET $REDIS_GET" >&3
+	redis_read
+	exec 3<&-
+	exec 3>&-
+	exit 0
+fi
+
+while read line
+do
+        REDIS_TODO=$line
+done < /dev/stdin
+
+read KEYNAME KEYVALUE <<<$(echo "$REDIS_TODO" | awk -F\= '{print $1,$2}')
+
+redis_compose_cmd "SET $KEYNAME $KEYVALUE" >&3
 echo "$(redis_read)"
 
 exec 3<&-
