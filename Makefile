6f8d1c01db34d99280bdaaa69d76ed19c8877be6
Pierre Fenoll <pierrefenoll@gmail.com>
2016-09-12 09:35:04 +0200

    tests: merge tests into a single file

--- Makefile
+++
@@ -1,5 +0,0 @@
-.PHONY: test
-
-test:
-	./test_str_var.sh
-	./test_array_var.sh
--- test.sh
+++ test.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -e
 
+# Test redi.sh
+
+
+## Test str var
+
+typeset Color='red'
+typeset | grep ^Color= | ./redi.sh
+
+out="$(./redi.sh -g Color)"
+
+[[ 'red' == "$out" ]]
+
+
+## Test array var
+
 typeset -a Colors=([0]='red' [1]='green' [2]='blue')
 typeset | grep ^Colors= | ./redi.sh -a
 
